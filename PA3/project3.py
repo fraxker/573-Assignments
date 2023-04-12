911757dd53b96c8e652a7d9b17789934a99e1be9
Andrew Sonnier <absonnie@ncsu.edu>
2023-04-12 16:32:04 +0000

    Added csv and readme

--- PA3/project3.py
+++ PA3/project3.py
@@ -1,9 +1,9 @@
 import rawsocketpy as rp
-import socket
 import signal
+import csv
 
 # create a raw socket
-raw_socket = rp.RawSocket('eth0', 0x0800)
+raw_socket = rp.RawSocket("eth0", 0x0800)
 
 # bind the socket to a network interface
 # raw_socket.bind('eth0')
@@ -20,10 +20,23 @@ http_cnt = 0
 https_cnt = 0
 quic_cnt = 0
 
+
 def done(signum, frame):
     print("Done!")
     print(ip_cnt, tcp_cnt, udp_cnt, dns_cnt, icmp_cnt, http_cnt, https_cnt, quic_cnt)
+    with open("sniffer_absonnie_ssgyurek_aecada2.csv", "w", newline='') as c:
+        csvwriter = csv.writer(c, delimiter=',')
+        csvwriter.writerow(["protocol","count"])
+        csvwriter.writerow(["ip", ip_cnt])
+        csvwriter.writerow(["tcp", tcp_cnt])
+        csvwriter.writerow(["udp", udp_cnt])
+        csvwriter.writerow(["dns", dns_cnt])
+        csvwriter.writerow(["icmp", icmp_cnt])
+        csvwriter.writerow(["http", http_cnt])
+        csvwriter.writerow(["https", https_cnt])
+        csvwriter.writerow(["quic", quic_cnt])
     exit(0)
+
 
 signal.signal(signal.SIGALRM, done)
 signal.alarm(30)
@@ -36,70 +49,42 @@ while True:
         packet = raw_socket.recv()
 
         str = rp.to_str(packet.data, separator="")
-        # print(int(str, base=16))
-        
-        assert str[0] == '4'
-        header_len = int(str[1], base=16)*4
+
+        assert str[0] == "4"
+        header_len = int(str[1], base=16) * 4
         assert header_len == 20
-        
-        # ToS 2-3
+
         total_len = int(str[4:8], base=16)
-        # identification 9-12
-        # ... ... ...
-        # protocol 18
         protocol = int(str[18:20], base=16)
-        data = str[header_len*2:total_len*2]  
-        data_len= len(data)/2
-        
-        # print(f"len(str/2) == {len(str)/2} && total_len = {total_len} && data_len = {data_len} && header_len = {header_len}")
-        
-        
-        # assert len(str)/2 == total_len 
-        # assert total_len == data_len+header_len
-        
-        ip_cnt += 1 
-        
-        # print(f"protocol = {protocol}")
+        data = str[header_len * 2 : total_len * 2]
+        data_len = len(data) / 2
+
+        ip_cnt += 1
+
         match protocol:
             case 6:
                 tcp_cnt += 1
-                
+
                 src_port = int(data[0:4], base=16)
-                # dest_port = int(data[4:8], base=16)
-                
+
                 match src_port:
                     case 80:
                         http_cnt += 1
                     case 443:
                         https_cnt += 1
-                    
-                
-                # print(f"src port = {src_port} && dest port = {dest_port}")
-                
-                
+
             case 17:
                 udp_cnt += 1
-                
+
                 src_port = int(data[0:4], base=16)
-                # dest_port = int(data[4:8], base=16)
-                
                 match src_port:
                     case 53:
                         dns_cnt += 1
                     case 443:
                         quic_cnt += 1
-                
-                # print(f"src port = {src_port} && dest port = {dest_port}")
-                        
             case 1:
                 icmp_cnt += 1
-            
 
-
-        # print packet contents
-        # print(rp.to_str(packet.data, separator=""))
-        # print(f"protocol = {protocol} && total_len = {int(str[4:8], base=16)}")
     except AssertionError as e:
         print(e)
         continue
-    
---
+++ PA3/readme_aecada2_ssgyurek_absonnie.txt
@@ -0,0 +1,8 @@
+Dependencies:
+
+In order to run this code, you must have:
+* Python 3.10, this version or greater is required due to the usage of the match statement
+* Rawsocketpy, this can be installed via that command "pip3 install rawsocketpy"
+
+Compilation and Execution:
+In order to run the code, run the command "python3 project3.py" which will then execute the program for 30 seconds and on exit generate the csv file.
